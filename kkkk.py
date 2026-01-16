0384381534ac0ebf002c8ba34ad6059297676055
trong <truongvantrong08112007@gmail.com>
2026-01-16 13:30:24 +0700

    Submit assignment 1

--- kkkk.py
+++
@@ -1,6 +0,0 @@
-hours = float(input('Enter Hours: '))
-rate = float(input('Enter Rate: '))
-x = 1.5
-amount = float(input('Enter amount of above hours:'))
-pay = hours * rate + amount * x * rate
-print(f'Pay: {pay}')
